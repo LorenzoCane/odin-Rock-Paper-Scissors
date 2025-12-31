a5f614ceb7629666df89f6963bb9fe57135b42c7
LorenzoCane <lorenzo.cane985@edu.unito.it>
2025-12-31 10:06:53 +0100

    Add comments

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,8 +1,11 @@
 // Rock Paper Scissors Gam,e (as part of Odin Project)
 
-//Const
-const log = console.log
+//----------------------------------------------------------------------------------
+//CONSTANTS
 
+//Console
+const log = console.log
+// W-L-D rules
 const rules = {
     "rock-scissors": "You Win! Rock beats Scissors",
     "rock-paper":   "You Lose! Paper beats Rock",
@@ -11,29 +14,31 @@ const rules = {
     "scissors-paper":"You Win! Scissors beats Paper",
     "scissors-rock": "You Lose! Rock beats Scissors",
 };
-
+//Win-lose-round counter
 let humanWin = 0;
 let computerWin = 0;
 let drawn = 0 ;
 let round = 0; 
 
+//----------------------------------------------------------------------------------
+//ELEMENTS
 
 //Buttons
 const buttonContainer =  document.querySelector(".buttonsContainer")
 const buttons = document.querySelectorAll("button");
+
 //Results box
 const resultsBox = document.querySelector(".resultsBox");
+//Princ Game recap
 const gameRecap = document.querySelector(".gameRecap");
 
-
-function buttonSelection(button){
-}
-
-//Possible Choice 
-let RPS_opt = ["rock", "paper", "scissors"]
+//----------------------------------------------------------------------------------
+//FUNCTIONS
 
 //Function to obtain computer choice: random # in [0;3] -> rounded -> choice
 function getComputerChoice(){
+    //possible choices
+    const RPS_opt = ["rock", "paper", "scissors"]
     let val = Math.floor(Math.random() * 3);
     return RPS_opt[val];
 }
@@ -41,33 +46,12 @@ function getComputerChoice(){
 //log(getComputerChoice());
 
 
-//Human Choice: staart the round when one of the button is clicked
-function getHumanChoice(){
-    
-
-    let choice = prompt("Enter 'Rock', 'Paper' or 'Scissors'");
-    choice = choice.toLowerCase();
-
-    if(!(RPS_opt.includes(choice))){
-        alert("Invalid choice, try again!")
-        return getHumanChoice();
-    }
-
-    return choice;
-}
-
-
-//log(getHumanChoice())
-
-//Round
+//Return the results of a round based on the selected human choice
 function playRound(human){
-    let roundResult = "";
     
     const computer = getComputerChoice();
-    //log("--------------------------------------------------");
-    //log("Computer Choice:" + computer);
-    //log("Human Choice:" + human); 
-    //Draw
+
+    //Drawn exit
     if (human === computer){
         roundResults = "Match drawn!";
         log(roundResults);
@@ -87,28 +71,36 @@ function playRound(human){
     return roundResults;
 }
 
+//----------------------------------------------------------------------------------
+//UI INTERACTION 
 
-//play a round
+//play a round every time a button is clicke
 buttons.forEach((button) => {
     button.addEventListener("click", function(e){
+        //Round results 
+        const resultsDisplay = document.createElement("p");
+        resultsDisplay.classList.add("resultsDisplay");
+        //Game Over message
         const finalWinner = document.createElement("h1");
         finalWinner.classList.add("finalWinner");
 
-        round += 1 ;
         const text = e.target.textContent.toLowerCase()
+        round += 1 ;
+
         let roundResults = playRound(text);
         
         const introContent = "Game Recap: " ;
         const content = introContent + "User: " +
             humanWin + ", " + "Computer: " + computerWin + ",  " +  "Drawn:" + drawn ;
+        
         log(content)
         gameRecap.textContent = content;
 
-        const resultsDisplay = document.createElement("p");
-        resultsDisplay.classList.add("resultsDisplay");
+
         resultsDisplay.textContent = "Round #"+ round + ": " + roundResults;
         resultsBox.appendChild(resultsDisplay);
 
+        //Game Over
         if(humanWin === 5){
             buttonContainer.replaceChildren()
             finalWinner.textContent = ("You win the game! \nCould you do it again? Refresh page!")
@@ -123,32 +115,3 @@ buttons.forEach((button) => {
     })
 
 });
-
-//Game
-function playFullGame(){
-
-
-    for (let i = 0; i < 5; i++) {
-        const humanchoice = getHumanChoice();
-        const computerChoice = getComputerChoice(); 
-
-        log("--------------------------------------------------");
-        log("Round #" + (i+1))
-        roundResults = playRound(humanchoice, computerChoice);
-        if (roundResults === "Match drawn!"){
-            drawn += 1 ;
-        
-        }else if(roundResults.startsWith("You Win")){
-            humanWin += 1;
-        }else{
-            computerWin += 1;
-        }
-    }   
-
-    log("*********************************************************");
-    log ("FINAL SCORE:") 
-    log("User: " + humanWin + ", " + "Computer: " + computerWin + ",  " +  "Drawn:" + drawn)
-        
-    return 0;
-}
-
